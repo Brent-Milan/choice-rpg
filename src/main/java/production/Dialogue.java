a914a0a0dea4515ed4bf43e7fc906aabab8b44b5
Brent-Milan <Bmilan24@gmail.com>
2017-08-07 23:52:14 -0400

    added placeholder art + tweaked Dialogue

--- src/main/java/production/Dialogue.java
+++ src/main/java/production/Dialogue.java
@@ -1,12 +1,19 @@
 package production;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Dialogue {
 
-	
+	@Id
+	@GeneratedValue
 	long id;
+	
+	@ManyToOne
+	Npc npc;
 	
 	/*
 	 * Research and make some design decisions before building this class out
